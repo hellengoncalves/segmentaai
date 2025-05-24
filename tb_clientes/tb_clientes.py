0c8cab28a8288981ae3a17e5da1369bab873bd25
Hellen Goncalves <hellenalcgoncalves@gmail.com>
2025-05-24 18:12:52 -0300

    "NEW TB"

--- tb_clientes/tb_clientes.py
+++ tb_clientes/tb_clientes.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
+from pyspark.sql.window import Window
 from airflow.hooks.base import BaseHook
 
 # 1. Sessão Spark
@@ -47,6 +48,60 @@ df_contratacoes_12m = (
     )
 )
 
+window_spec = Window.partitionBy("CD_CLIENTE")
+
+df_dados_gerais = (
+    spark.read
+    .option("header", "true") 
+    .option("delimiter", ";") 
+    .option("inferSchema", "true") 
+    .option("encoding", "UTF-8")
+    .csv("s3://segmentaai/dados_clientes.csv")
+    .filter(
+        (F.col('CD_CLIENTE').isNotNull()) &
+        (F.col('CIDADE').isNotNull()) &
+        (F.col('CD_CLIENTE') != 'TDCG0P')
+    )
+    .select(
+        F.col("CD_CLIENTE").alias("cd_cliente"),
+        F.coalesce(F.col("CIDADE"), F.lit('-')).alias("nm_cidade"),
+        F.coalesce(F.col("DS_CNAE"), F.lit('-')).alias("ds_cnae"),
+        F.coalesce(F.col("DS_SEGMENTO"), F.lit('-')).alias("ds_segmento"),
+        F.coalesce(F.col("DS_SUBSEGMENTO"), F.lit('-')).alias("ds_subsegmento"),
+        F.coalesce(F.col("FAT_FAIXA"), F.lit('-')).alias("vl_faixa_faturamento"),
+        F.coalesce(F.col("PAIS"), F.lit('-')).alias("nm_pais"),
+        F.coalesce(F.col("UF"), F.lit('-')).alias("nm_uf"),
+        F.row_number().over(window_spec).alias('rn')
+    ).distinct()
+)
+
+df_dados_gerais_filtro = (
+    spark.read
+    .option("header", "true") 
+    .option("delimiter", ";") 
+    .option("inferSchema", "true") 
+    .option("encoding", "UTF-8")
+    .csv("s3://segmentaai/dados_clientes.csv")
+    .filter(
+        (F.col('CD_CLIENTE').isNotNull()) &
+        (F.col('CIDADE').isNotNull()) &
+        (F.col('CD_CLIENTE') == 'TDCG0P')
+    )
+    .select(
+        F.col("CD_CLIENTE").alias("cd_cliente"),
+        F.coalesce(F.col("CIDADE"), F.lit('-')).alias("nm_cidade"),
+        F.coalesce(F.col("DS_CNAE"), F.lit('-')).alias("ds_cnae"),
+        F.coalesce(F.col("DS_SEGMENTO"), F.lit('-')).alias("ds_segmento"),
+        F.coalesce(F.col("DS_SUBSEGMENTO"), F.lit('-')).alias("ds_subsegmento"),
+        F.coalesce(F.col("FAT_FAIXA"), F.lit('-')).alias("vl_faixa_faturamento"),
+        F.coalesce(F.col("PAIS"), F.lit('-')).alias("nm_pais"),
+        F.coalesce(F.col("UF"), F.lit('-')).alias("nm_uf"),
+        F.row_number().over(window_spec).alias('rn')
+    ).distinct()
+)
+
+df_dados_completos = (df_dados_gerais.unionAll(df_dados_gerais_filtro).select(F.col('*')).distinct())
+
 df_consolidado = (
     df_base_clientes.alias('a')
     .join(
@@ -54,11 +109,24 @@ df_consolidado = (
         ['cd_cliente'],
         'left'
     )
+    .join(
+        df_dados_completos.alias('c'),
+        (F.col('a.cd_cliente') == F.col('c.cd_cliente')) & (F.col('c.rn') == 1),
+        'left'
+    )
+    .filter(F.col('c.rn') == 1)
     .select(
         F.col('a.cd_cliente'),
         F.col('a.dt_abertura'),
         F.coalesce(F.col('b.qt_contratacao_12m'), 0).alias('qt_contratacao_12m'),
-        F.coalesce(F.col('b.vl_contratacao_12m'), 0).alias('vl_contratacao_12m')
+        F.coalesce(F.col('b.vl_contratacao_12m'), 0).alias('vl_contratacao_12m'),
+        F.coalesce(F.col("c.nm_cidade"), F.lit('-')).alias("nm_cidade"),
+        F.coalesce(F.col("c.ds_cnae"), F.lit('-')).alias("ds_cnae"),
+        F.coalesce(F.col("c.ds_segmento"), F.lit('-')).alias("ds_segmento"),
+        F.coalesce(F.col("c.ds_subsegmento"), F.lit('-')).alias("ds_subsegmento"),
+        F.coalesce(F.col("c.vl_faixa_faturamento"), F.lit('-')).alias("vl_faixa_faturamento"),
+        F.coalesce(F.col("c.nm_pais"), F.lit('-')).alias("nm_pais"),
+        F.coalesce(F.col("c.nm_uf"), F.lit('-')).alias("nm_uf"),
     )
 )
 
