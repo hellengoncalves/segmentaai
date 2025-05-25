77e6b6a8f37d0b60e2d6204bf47d668cb4ddfe30
Hellen Goncalves <hellenalcgoncalves@gmail.com>
2025-05-25 16:39:35 -0300

    "utf"

--- tb_nps_tran_produto/tb_nps_tran_produto.py
+++ tb_nps_tran_produto/tb_nps_tran_produto.py
@@ -26,9 +26,8 @@ df_nps_tran_produto = (
     .option("header", "true") 
     .option("delimiter", ";") 
     .option("inferSchema", "true") 
-    .option("encoding", "UTF-8")
+    .option("encoding", "ISO-8859-1")
     .csv("s3a://segmentaai/nps_transacional_produto.csv")
-    .withColumnRenamed("C�d. T", "Cód. T")
     .filter(
         (F.col('Cód. T').isNotNull())
     )
